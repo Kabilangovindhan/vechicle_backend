b39cc11f8b802737fe3a2c5a7f88be18e5db3c30
Kabilangovindhan <kabilang03@gmail.com>
2026-02-09 15:13:46 +0530

    customerinfo

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const UserModel = require("./models/user");
 
+
 dotenv.config();
 connectDB();
 
@@ -15,16 +16,56 @@ app.use(express.json());
 
 /* ✅ GET ALL USERS (VEHICLES DATA) */
 app.get("/api/users", async (req, res) => {
-  try {
-    const users = await UserModel.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const users = await UserModel.find();
+		res.json(users);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 );
+
+app.delete("/api/users/:id", async (req, res) => {
+	try {
+		await UserModel.findByIdAndDelete(req.params.id);
+		res.json({ message: "Deleted" });
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: err.message });
+
+	}
+});
+
+
+app.post("/api/users", async (req, res) => {
+
+	try {
+		const newUser = await UserModel.create(req.body);
+		res.json(newUser);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: err.message });
+	}
+});
+
+
+app.put("/api/users/:id", async (req, res) => {
+
+	try {
+		const updated = await UserModel.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ new: true }
+		);
+
+		res.json(updated);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
